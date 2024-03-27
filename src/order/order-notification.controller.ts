097a174064f34e378f37761e443713b167f3816b
Mateus Deitos <mateus.deitos@tiny.com.br>
2024-03-26 22:47:58 -0300

    organizando

--- src/order/order-notification.controller.ts
+++ src/order/order-notification.controller.ts
@@ -1,38 +1,61 @@
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
-import { Body, Controller, HttpCode, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Inject,
+  Param,
+  Post,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { randomBytes } from 'crypto';
-import { FromNotificationAdapter } from './adapters/from-notification.adapter';
+import { TOrdersModule } from './types';
 
 @Controller('order-notification')
 export class OrderNotificationController {
   constructor(
-    private readonly adapter: FromNotificationAdapter,
     private readonly amqpConnection: AmqpConnection,
+    @Inject('GetOrderWorker')
+    private readonly worker: TOrdersModule.GetOrderInterface<any, any>,
   ) {}
 
   @HttpCode(200)
   @Post(':platform')
-  public async handle(@Body() content, @Param('platform') platform: string) {
-    const message = await this.adapter.parseNotification({
-      platform,
-      content,
-    });
-
+  public async handle(
+    @Body() notification: TOrdersModule.ParsedOrderNotification<any>,
+    @Param('platform') platform: string,
+  ) {
     const uniqueId = randomBytes(16).toString('hex');
 
-    const envelope = {
-      uniqueId,
-      message,
-    };
+    await this.amqpConnection.publish(
+      'orders_receive_notification_exchange',
+      `orders.notification.${platform}`,
+      { uniqueId, step: 'order_notification_received', content: notification },
+    );
+
+    const order = await this.worker.getOrderFromNotification(notification);
 
     const result = await this.amqpConnection.publish(
       'orders_receive_notification_exchange',
       `orders.new.${platform}`,
-      envelope,
+      { uniqueId, step: 'order_obtained_from_platform', content: order },
+      {
+        headers: {
+          'x-delay': 10000, // precisa de plugin de delay no RabbitMQ
+        },
+      },
     );
 
+    if (!result) {
+      throw new UnprocessableEntityException(
+        'Order could not be sent to processing queue',
+      );
+    }
+
     return {
-      result,
+      uniqueId,
+      message: 'Order obtained from platform and sent to processing queue',
+      order,
     };
   }
 }
--- src/order/workers/order.history.worker.ts
+++ src/order/workers/order.history.worker.ts
@@ -1,27 +1,24 @@
 import {
-  Nack,
   RabbitPayload,
   RabbitRequest,
   RabbitSubscribe,
 } from '@golevelup/nestjs-rabbitmq';
-import { ParsedNotification } from 'src/order/adapters/from-notification.adapter';
 
 export class OrderHistoryWorker {
   @RabbitSubscribe({
     exchange: 'orders_receive_notification_exchange',
     queue: 'queue_orders_history',
-    routingKey: 'orders.*',
+    routingKey: 'orders.#',
   })
   async process(
-    @RabbitPayload() message: ParsedNotification,
+    @RabbitPayload() envelope: Record<string, any>,
     @RabbitRequest() amqpMsg,
   ) {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
     console.log(
       'OrderHistoryWorker',
       JSON.stringify(
         {
-          message,
+          envelope,
           fields: amqpMsg.fields,
           properties: amqpMsg.properties,
         },

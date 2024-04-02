cdf0fe30c16aa311d156af866c2283fefae18f16
Mateus Deitos <mateus.deitos@tiny.com.br>
2024-04-01 22:23:36 -0300

    refactor: modularizando integracao com ML

--- src/modules/integration/integration.module.ts
+++ src/modules/integration/integration.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DrizzleDatabaseClient } from 'src/providers/database.client';
-import { MercadoLivreRestClient } from 'src/providers/platform.handlers/mercadolivre/rest.client';
-import { MercadoLivreTestAuth } from 'src/providers/platform.handlers/mercadolivre/test.auth';
-import { UserApi } from 'src/providers/platform.handlers/mercadolivre/user.api';
+import { MercadoLivreModule } from 'src/providers/platform.handlers/mercadolivre/mercadolivre.module';
 import { IntegrationAuthRepository } from '../integration.auth/db/integration.auth.repository';
 import { IntegrationRepository } from './db/integration.repository';
 import { IntegrationController } from './http/integration.controller';
@@ -14,14 +12,14 @@ import { TestAuthIntegrationUseCase } from './usecases/test.auth.integration.use
   controllers: [IntegrationController],
   providers: [
     CreateIntegrationUseCase,
-    TestAuthFactory,
     TestAuthIntegrationUseCase,
+
     IntegrationRepository,
     IntegrationAuthRepository,
     DrizzleDatabaseClient,
-    MercadoLivreTestAuth,
-    UserApi,
-    MercadoLivreRestClient,
+
+    TestAuthFactory,
   ],
+  imports: [MercadoLivreModule],
 })
 export class IntegrationModule {}
